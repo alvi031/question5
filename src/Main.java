83520db893c67181940d159415374d282c32be8c
alvi031 <alvi,ismailov095@gmail.com>
2025-10-17 01:00:48 +0300

    .

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int[][] triangle = {{-1},
+                {2, 3},
+                {1, -1, -3},
+                {4,  2,  1, 3}
+        };
+        System.out.println(minPath(triangle));
+    }
+
+    public static int minPath(int[][] triangle) {
+        int[] fin = triangle[triangle.length - 1].clone();
+
+        for (int i = triangle.length - 2; i >= 0; i--) {
+            for (int j = 0; j < triangle[i].length; j++) {
+                System.out.println("fin["+j +"] = " + (triangle[i][j] + Math.min(fin[j], fin[j + 1])));
+                fin[j] = triangle[i][j] + Math.min(fin[j], fin[j + 1]);
+                System.out.println(Arrays.toString(fin));
+            }
         }
+        return fin[0];
     }
 }
